6ce7e70d388b17786553e322d955f1cc2def35e8
Daiyan Hussain <Daiyanh920@gmail.com>
2025-03-03 18:03:57 -0500

    In-class-14

---
+++ In-class-14/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require 'php4/Item.php';
+
+use php4/Item;
+
+$Item = new Item("Fried Rice", 5.00);
+
+echo $Item->business();
+
+
+?>
---
+++ In-class-14/php4/Item.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace php4; 
+
+class Item{
+
+    public $name;
+    public $price;
+
+    public function __contruct($name, $price)
+    {
+        $this->name = $name;
+        $this->price = $price;
+    } 
+
+    public function business()
+    {       
+        $food = 
+        [
+            "Fried Rice" => '5.00',
+            "Chicken Fried Rice" => '6.00'
+        ];
+
+        return json_encode($food);
+    }
+}
+
+
+?>
